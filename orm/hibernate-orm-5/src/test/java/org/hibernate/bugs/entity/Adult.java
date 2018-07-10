2cbf13640f70e60364037fc97eacfef124aba15d
Oliver Libutzki <oliver.libutzki@bmiag.de>
2018-07-10 13:51:17 +0200

    Unnecessary annotation removed
    Signed-off-by: Oliver Libutzki <oliver.libutzki@bmiag.de>

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/entity/Adult.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/entity/Adult.java
@@ -1,10 +1,8 @@
 package org.hibernate.bugs.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "adult")
 public class Adult extends Person {
 
 	public Adult() {
